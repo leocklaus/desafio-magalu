9d52c6c9980fbd84e0dd9326f44a22c049f2567d
leocklaus <leocklaus@gmail.com>
2024-07-20 09:00:59 -0300

    feat: project created

---
+++ src/test/java/io/github/leocklaus/desafiomagalu/DesafiomagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.desafiomagalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiomagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
